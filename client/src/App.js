97f59553525dd93635cfe63f59726cc74270bd8a
Sithanga <sithanga.uwage@gmail.com>
2020-11-07 14:47:09 +0000

    Displaying basic Jobs Page with hard-coded jobs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import Jobs from './Jobs';
+
+const mockJobs = [
+  {title: 'SWE 1', company: 'Google'},
+  {title: 'SWE 1', company: 'Apple'},
+  {title: 'SWE 1', company: 'Facebook'},
+]
+
 function App() {
   return (
     <div className="App">
-      
+      <Jobs jobs= {mockJobs}/>
     </div>
   );
 }
