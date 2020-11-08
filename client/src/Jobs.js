6aaf450c3a4e4e8172539f00fb7a30fae388b24f
Sithanga <sithanga.uwage@gmail.com>
2020-11-08 11:46:15 +0000

    Adding stepper for navigation of page

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -3,7 +3,25 @@ import Typography from '@material-ui/core/Typography';
 
 import Job from './Job';
 
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
 export default function Jobs({jobs}) {
+
+  const numJobs = jobs.length;
+  const numPages = Math.ceil(numJobs / 50)
+  const [activeStep, setActiveStep] = React.useState(0);
+  const jobsOnPage = jobs.slice(activeStep * 50, (activeStep * 50) + 50);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
   console.log('jobs is', jobs[0]);
 
@@ -12,11 +30,35 @@ export default function Jobs({jobs}) {
       <Typography variant="h4" component="h1">
         Entry Level Software Jobs
       </Typography>
+      <Typography variant="h6" component="h1">
+        Found {numJobs} Jobs
+      </Typography>
       {
-        jobs.map(
+        jobsOnPage.map(
           job => <Job job = {job} />
         )
       }
+      <div>
+        Page {activeStep + 1} of {numPages}
+      </div>
+      <MobileStepper
+        variant="progress"
+        steps={numPages}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === (numPages - 1)}>
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
     </div>
   )
 }
