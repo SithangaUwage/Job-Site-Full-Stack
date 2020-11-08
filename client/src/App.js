5a64ab9fe029eb8cc9edac0738ce211b4412b646
Sithanga <sithanga.uwage@gmail.com>
2020-11-08 09:20:59 +0000

    Link the api data with the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import fetch from 'node-fetch';
 import React from 'react';
 import './App.css';
 
 import Jobs from './Jobs';
+
+const JOB_API_URL = 'http://localhost:3001/jobs'
 
 const mockJobs = [
   {title: 'SWE 1', company: 'Google'},
@@ -9,10 +12,24 @@ const mockJobs = [
   {title: 'SWE 1', company: 'Facebook'},
 ]
 
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  updateCb(json)
+}
+
 function App() {
+
+  const [jobList, updateJobs] = React.useState([])
+
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  }, [])
+
   return (
     <div className="App">
-      <Jobs jobs= {mockJobs}/>
+      <Jobs jobs= {jobList}/>
     </div>
   );
 }
